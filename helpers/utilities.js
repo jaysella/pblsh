735a516eaad48416f99acee672de7e7f97b0a37a
Jay Sella <jay@jaysella.dev>
2021-06-16 19:47:32 -0400

    commit missing isEquivalent() helper

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -7,3 +7,26 @@ export function disabledEventPropagation(e) {
     }
   }
 }
+
+// Thanks to http://adripofjavascript.com/blog/drips/object-equality-in-javascript.html
+export function isEquivalent(a, b) {
+  // Create arrays of property names
+  var aProps = Object.getOwnPropertyNames(a);
+  var bProps = Object.getOwnPropertyNames(b);
+
+  // If number of properties is different,
+  // objects are not equivalent
+  if (aProps.length != bProps.length) {
+    return false;
+  }
+
+  for (var i = 0; i < aProps.length; i++) {
+    var propName = aProps[i];
+
+    // If values of same property are not equal,
+    // objects are not equivalent
+    if (a[propName] !== b[propName]) {
+      return false;
+    }
+  }
+}
